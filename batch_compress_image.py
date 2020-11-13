2163d7c79ea3ece28f43b3b15a33f7a3620bc8cd
alphardex <2582347430@qq.com>
2020-11-13 17:06:17 +0800

    update

--- batch_compress_image.py
+++ batch_compress_image.py
@@ -4,11 +4,29 @@
 from pathlib import Path
 from PIL import Image
 
-ext = 'png'
-thumbsize = (240, 120)
+ext = 'jpg'
+thumb_size = (240, 120)
+thumb_ratio = 10
 
 images = [path for path in Path('.').glob(f'*.{ext}')]
-for image in images:
-    im = Image.open(image)
-    im.thumbnail(thumbsize)
-    im.save(image)
+
+
+def compress_fixed_size():
+    for image in images:
+        im = Image.open(image)
+        im.thumbnail(thumb_size)
+        im.save(image)
+
+
+def compress_fixed_ratio():
+    for (i, image) in enumerate(images):
+        im = Image.open(image)
+        w, h = im.size
+        print(w, h)
+        nw, nh = int(w/thumb_ratio), int(h/thumb_ratio)
+        print(nw, nh)
+        thumbnail = im.resize((nw, nh), Image.ANTIALIAS)
+        thumbnail.save(f'{i}.jpg')
+
+
+compress_fixed_ratio()
