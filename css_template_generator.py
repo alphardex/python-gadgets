5bc32ba31eacf5b5b7b3d76497d024b29876fbfc
alphardex <2582347430@qq.com>
2020-11-28 19:18:44 +0800

    update

--- css_template_generator.py
+++ css_template_generator.py
@@ -9,7 +9,7 @@ HTML_TEMPLATE = """<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@alphardex/aqua.css@1.5.6/dist/aqua.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@alphardex/aqua.css@1.6.0/dist/aqua.min.css" />
     <link rel="stylesheet" href="style.css" />
   </head>
   <body>
